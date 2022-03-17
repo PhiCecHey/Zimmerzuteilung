fd1f68507d459768535950dcd2aeff2e333b1c9f
PhiCecHey <philine.cecilia.heyder@uni-weimar.de>
2022-03-17 06:06:33 +0100

    init Zimmer

--- app/src/main/java/zimmerzuteilung/Zimmer.java
+++ app/src/main/java/zimmerzuteilung/Zimmer.java
@@ -1,6 +1,31 @@
 package zimmerzuteilung;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.checkerframework.checker.units.qual.s;
+
 public class Zimmer {
     int nummer;
+    Map<UUID, Schueler> bewohner = new HashMap<>();
 
+    Zimmer(Internat internat, int nummer) throws IllegalArgumentException{
+        if(nummer < 0){
+            throw new IllegalArgumentException();
+        }
+        this.nummer = nummer;
+        internat.addZimmer(this);
+    }
+
+    boolean addSchueler(Schueler schueler){
+        for(Map.Entry<UUID, Schueler> eintrag : this.bewohner.entrySet()){
+            if(eintrag.getKey() == schueler.ID){
+                return false;
+            }
+        }
+
+        this.bewohner.put(schueler.ID, schueler);
+        return true;
+    }
 }
