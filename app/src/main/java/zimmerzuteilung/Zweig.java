9ab519138c966bcc909cb40bc9be81a5756281fc
PhiCecHey <philine.cecilia.heyder@uni-weimar.de>
2022-03-17 06:06:45 +0100

    init Zweig

--- app/src/main/java/zimmerzuteilung/Zweig.java
+++ app/src/main/java/zimmerzuteilung/Zweig.java
@@ -3,20 +3,20 @@ package zimmerzuteilung;
 import java.util.Map;
 import java.util.HashMap;
 
-public class Zweig{
-    public enum enumZweig{
+public class Zweig {
+    public enum BEZEICHNUNG {
         MUSIK, SPRACHEN, NAWI;
-    }    
+    }
 
-    enumZweig bezeichung;
-    Map<Klasse.enumKlasse, Klasse> klassen = new HashMap<>();
+    BEZEICHNUNG bezeichung;
+    Map<Klasse.BEZEICHNUNG, Klasse> klassen = new HashMap<>();
 
-    Zweig(enumZweig bezeichnung){
+    Zweig(BEZEICHNUNG bezeichnung) {
         this.bezeichung = bezeichnung;
     }
 
-    boolean klasseHinzufuegen(Klasse klasse){
-        for(Map.Entry<Klasse.enumKlasse, Klasse> eintrag : this.klassen.entrySet()){
+    boolean addKlasse(Klasse klasse){
+        for(Map.Entry<Klasse.BEZEICHNUNG, Klasse> eintrag : this.klassen.entrySet()){
             if(eintrag.getKey() == klasse.bezeichnung){
                 return false;
             }
@@ -25,4 +25,27 @@ public class Zweig{
         this.klassen.put(klasse.bezeichnung, klasse);
         return true;
     }
+
+    public void initKlassen() {
+        switch (this.bezeichung) {
+            case MUSIK:
+                this.klassen.put(Klasse.BEZEICHNUNG.neunM, new Klasse(Klasse.BEZEICHNUNG.neunM));
+                this.klassen.put(Klasse.BEZEICHNUNG.zehnM, new Klasse(Klasse.BEZEICHNUNG.zehnM));
+                this.klassen.put(Klasse.BEZEICHNUNG.elfM, new Klasse(Klasse.BEZEICHNUNG.elfM));
+                this.klassen.put(Klasse.BEZEICHNUNG.zwoelfM, new Klasse(Klasse.BEZEICHNUNG.zwoelfM));
+            case SPRACHEN:
+                this.klassen.put(Klasse.BEZEICHNUNG.neunS, new Klasse(Klasse.BEZEICHNUNG.neunS));
+                this.klassen.put(Klasse.BEZEICHNUNG.zehnS, new Klasse(Klasse.BEZEICHNUNG.zehnS));
+                this.klassen.put(Klasse.BEZEICHNUNG.elfS, new Klasse(Klasse.BEZEICHNUNG.elfS));
+                this.klassen.put(Klasse.BEZEICHNUNG.zwoelfS, new Klasse(Klasse.BEZEICHNUNG.zwoelfS));
+            case NAWI:
+                this.klassen.put(Klasse.BEZEICHNUNG.neunN, new Klasse(Klasse.BEZEICHNUNG.neunN));
+                this.klassen.put(Klasse.BEZEICHNUNG.zehnN, new Klasse(Klasse.BEZEICHNUNG.zehnN));
+                this.klassen.put(Klasse.BEZEICHNUNG.elfN, new Klasse(Klasse.BEZEICHNUNG.elfN));
+                this.klassen.put(Klasse.BEZEICHNUNG.zwoelfN, new Klasse(Klasse.BEZEICHNUNG.zwoelfN));
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
 }
