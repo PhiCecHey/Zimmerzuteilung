ada67c9beedf1cab32c37f5c582d8b667e6ae063
PhiCecHey <philine.cecilia.heyder@uni-weimar.de>
2022-03-19 17:25:58 +0100

    Schüler werden durchgezählt und erhalten somit ihre ID statt eine ID zu generieren

--- app/src/main/java/zimmerzuteilung/Schueler.java
+++ app/src/main/java/zimmerzuteilung/Schueler.java
@@ -1,19 +1,26 @@
 package zimmerzuteilung;
 
-import java.util.UUID;
+//import java.util.UUID;
 
 public class Schueler {
     public enum Geschlecht {
         m, w, d
     }
 
+    /* Die Schueler werden der Reihe nach durchgezaehlt und erhalten somit ihre ID. 
+    Vorteil zur randomID: verbraucht weniger Platz und ist effizienter im Vergleich. */
+    static int anzahl = 0; 
+
     String name;
     Geschlecht geschlecht;
-    final UUID ID = UUID.randomUUID();
+    final int ID;
 
     public Schueler(String name, Geschlecht geschlecht){
         this.name = name;
         this.geschlecht = geschlecht;
+
+        ++ Schueler.anzahl;  // der erste Schueler kriegt also Nr. 1
+        this.ID = Schueler.anzahl;
     }
 
 }
