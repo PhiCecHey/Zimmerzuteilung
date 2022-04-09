0577cef94d4f5634f92ccde9e24143da547a7eb9
PhiCecHey <philine.cecilia.heyder@uni-weimar.de>
2022-04-09 23:41:37 +0200

    Zimmer haben jetzt eine Kapazität

--- app/src/main/java/zimmerzuteilung/objekte/Zimmer.java
+++ app/src/main/java/zimmerzuteilung/objekte/Zimmer.java
@@ -9,24 +9,31 @@ public class Zimmer {
 
     final int ID;
     String nummer;
+    int kapazitaet;
     Map<Integer, Schueler> bewohner = new HashMap<>();
 
-    Zimmer(Internat internat) throws IllegalArgumentException{
+    Zimmer(Internat internat, int kapazitaet) throws IllegalArgumentException{
         ++Zimmer.anzahl;
         this.ID = Zimmer.anzahl;
+        this.kapazitaet = kapazitaet;
 
         internat.addZimmer(this);
     }
 
-    Zimmer(Internat internat, String nummer) throws IllegalArgumentException{
+    Zimmer(Internat internat, String nummer, int kapazitaet) throws IllegalArgumentException{
         ++Zimmer.anzahl;
         this.ID = Zimmer.anzahl;
+        this.kapazitaet = kapazitaet;
 
         internat.addZimmer(this);
         this.nummer = nummer;
     }
 
     boolean addSchueler(Schueler schueler){
+        if (this.kapazitaet <= this.bewohner.size()){
+            return false;
+        }
+
         for(Map.Entry<Integer, Schueler> eintrag : this.bewohner.entrySet()){
             if(eintrag.getKey() == schueler.ID){
                 return false;
